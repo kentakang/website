e89830e2a29e3f36f32cc534e88ffd522be8a5ac
Chan Kang <me@kentakang.com>
2025-01-31 12:24:31 +0900

    feat: add deploy to github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kentakang.github.io',
+  base: 'website',
   vite: {
     plugins: [tailwindcss()]
   }
